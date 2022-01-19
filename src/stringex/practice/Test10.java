d4e8c25d4164e8c8c969b4fbe916e8904fd7fdc7
tjdgus <tjdgus7562@naver.com>
2022-01-20 01:42:20 +0900

    파싱

--- src/stringex/practice/Test10.java
+++ src/stringex/practice/Test10.java
@@ -3,7 +3,9 @@ package stringex.practice;
 public class Test10 {
     public static void main(String[] args) {
         String str = "ABCDEF";
-        String substring = str.substring(0, 2);
-        System.out.println("substring: " + substring);
+        String substring1 = str.substring(0, 2);
+        String substring2 = str.substring(2);
+        System.out.println("substring: " + substring1);
+        System.out.println("substring: " + substring2);
     }
 }
